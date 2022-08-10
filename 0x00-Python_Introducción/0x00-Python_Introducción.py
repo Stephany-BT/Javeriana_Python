bd0b63d809e9d2484c071397f51f7461f3068dc8
Stephany-BT <110844423+Stephany-BT@users.noreply.github.com>
2022-08-10 10:53:44 -0500

    Create 0x00-Python_Introducción.py

---
+++ 0x00-Python_Introducción/0x00-Python_Introducción.py
@@ -0,0 +1,9 @@
+
+
+# Actividad #1
+# Stephany Betancourt T.
+# Codigo: 8976103
+
+print("Hola mundo")
+print("Mi nombre es Stephany Betancourt Torres")
+print("Hoy es Martes 02/08/2022 y son las 21:49")
