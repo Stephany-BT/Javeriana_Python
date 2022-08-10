ac9b69d03050ce3b88af7f2530c7513077cce4cd
Stephany-BT <110844423+Stephany-BT@users.noreply.github.com>
2022-08-10 10:51:10 -0500

    Create Actividad #5.py

---
+++ 0x05-Functions.py/Actividad #5.py
@@ -0,0 +1,23 @@
+#!/usr/bin/Python3
+
+#Definir función
+def suma(num1,num2):
+    print("Resultado de la suma:" , num1+num2)
+def resta(var_1,var_2):
+    print("Resultado de la resta:" , var_1 - var_2)
+def multi(mul1,mul2):
+    print("Resultado de la resta:" , mul1 *mul2)
+def poten(num , exp):
+    print("Resultado de la potancia:" , num**exp)
+
+#Llamar la función
+suma(3,8)
+print("__________________________________________")
+resta(5,11)
+print("------------------------------------------")
+multi(4,8)
+print("------------------------------------------")
+poten(5,2)
+print("------------------------------------------")
+
+#Fin del codigo
